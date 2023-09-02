28ffb870ada2bee24d027daf800e49d9eb32a556
bitecode <nfdsmwz@gmail.com>
2023-09-02 08:07:43 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "transfer.sh",
-      script: "go run main.go --provider=local --listener :9000 --temp-path=/tmp/ --basedir=/tmp/ --cors-domains * --purge-days 7",
+      script: "go run main.go --provider=local --listener :9000 --temp-path=~/tmp/ --basedir=~/tmp/ --cors-domains * --purge-days 7",
       cwd: __dirname,
       instances: 1,
     },
