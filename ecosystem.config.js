a4e5953e974cc9f8bc9ea49fde78e9abfaf5c11a
bitecode <nfdsmwz@gmail.com>
2023-09-02 08:05:56 +0800

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "transfer.sh",
+      script: "go run main.go --provider=local --listener :9000 --temp-path=/tmp/ --basedir=/tmp/",
+      cwd: __dirname,
+      instances: 1,
+    },
+  ],
+};
